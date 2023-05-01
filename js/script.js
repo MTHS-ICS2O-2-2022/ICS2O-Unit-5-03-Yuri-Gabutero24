55008f7ba3e7d94aa7cdfc9ad2f0fcb1a63d66f4
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-01 17:25:34 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -8,21 +8,22 @@
 /**
  * This function checks to see if the user is old enough to see certain movies
  */
-function myButtonClicked () {
+function myButtonClicked() {
   //input
   const age = parseInt(document.getElementById("age").value)
 
   //process and output
   if (age >= 18) {
-    document.getElementById("rating").innerHTML = "You can watch R rated movies alone."
-  }
-  else if (age >= 13  && age < 18) {
-    document.getElementById("rating").innerHTML = "You can watch PG-13 rated movies alone.";
-  }
-  else if (age >= 5 && age < 13) {
-    document.getElementById("rating").innerHTML = "You can watch G and/or PG rated movies alone.";
-  }
-  else {
-    document.getElementById("rating").innerHTML = "You're probably too young to do most things.";
+    document.getElementById("rating").innerHTML =
+      "You can watch R rated movies alone."
+  } else if (age >= 13 && age < 18) {
+    document.getElementById("rating").innerHTML =
+      "You can watch PG-13 rated movies alone."
+  } else if (age >= 5 && age < 13) {
+    document.getElementById("rating").innerHTML =
+      "You can watch G and/or PG rated movies alone."
+  } else {
+    document.getElementById("rating").innerHTML =
+      "You're probably too young to do most things."
   }
 }
